f655ce383bcc96e076e192289452ecf5ec0120af
ekemini_udofia <ekeminiabasiudofia@gmail.com>
2025-02-24 20:39:59 +0100

    added the main python files for the project

---
+++ src/main.py
@@ -0,0 +1,18 @@
+from tkinter import*
+from pygame import mixer
+
+root = Tk()
+
+root.title("Media Player")
+
+root.resizable(True, True)    
+
+
+def play_video():
+    return 0 #change this later
+    
+
+def play_music():
+    return 0 #change this later
+
+root.mainloop()
